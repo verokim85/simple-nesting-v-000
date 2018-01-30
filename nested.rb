2e22cae000c85c67f1f81b23bba9d902a968e56a
Veronica Kim <vero.a.kim@gmail.com>
2018-01-30 23:32:55 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -104,8 +104,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     programmer_hash[:alan_kay][:known_for] = "GUI"
-
+    x = programmer_hash[:alan_kay][:known_for] = "GUI"
+		return x
 
 end
 
