59f2d1c527291cf141af1a5cafd772fb0ba8df67
Veronica Kim <vero.a.kim@gmail.com>
2018-01-30 23:08:46 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -66,7 +66,7 @@ def adding_matz
 
 	programmer_hash =
  		{
-				:yukihiro_matsumoto => {}
+				:yukihiro_matsumoto => {},
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
