5d9aed440f01dbf04934bd5b8fa0ed86f9707438
Veronica Kim <vero.a.kim@gmail.com>
2018-01-30 23:16:36 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -23,7 +23,7 @@ def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 
 	programmer_hash =
- 		{:Object Orientation => {
+ 		{
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -35,8 +35,7 @@ def alan_kay_is_known_for
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-				}
-				}
+        }
      }
 end
 
