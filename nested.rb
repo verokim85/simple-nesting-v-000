f840c6d1c7b77dd061077192eac824a1d290e939
Veronica Kim <vero.a.kim@gmail.com>
2018-01-30 23:22:47 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -53,7 +53,7 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C".join]
+          :languages => ["C"]
         }
      }
 		 programmer_hash[:dennis_ritchie][:languages]
